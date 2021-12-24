5f81ee6c154df335e12c12e9226910cbcd80417a
renosatyaadrian <renosatyaadrian@gmail.com>
2021-12-24 07:34:23 +0700

    remove unused lib

--- SampleRESTAPI/Controllers/StudentsController.cs
+++ SampleRESTAPI/Controllers/StudentsController.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SampleRESTAPI.Data;
 using SampleRESTAPI.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace SampleRESTAPI.Controllers
